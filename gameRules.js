3bcf4130e3a45244c6b6c92a1128bfa1b3e8092c
Mariela <mvmihilova@gmail.com>
2022-10-18 06:25:17 -0600

    initial

---
+++ gameRules.js
@@ -0,0 +1,11 @@
+export function gameRules () {
+    console.log (
+      ` \n
+      GAME RULES:
+     1. Use small letters only;
+     2. One word movie titles are used;
+     3. You win one point for every title you guess`
+        
+    );
+  }
+  gameRules()
