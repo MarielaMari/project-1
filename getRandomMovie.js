b3ad2a09774b56d350515392811706af082c7bbc
Mariela <mvmihilova@gmail.com>
2022-10-24 22:50:07 -0600

    moved to src folder

--- getRandomMovie.js
+++
@@ -1,5 +0,0 @@
-export function getRandomMovie (movieChoices) {
-    const length = movieChoices.length;
-    const randomIndex = Math.floor (Math.random () * length);
-    return movieChoices[randomIndex];
-  }
