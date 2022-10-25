2ecbd92a24ef4a0a9764acbc45dc2208529af927
Mariela <mvmihilova@gmail.com>
2022-10-24 22:43:14 -0600

    delete

--- gameRules.js
+++
@@ -1,9 +0,0 @@
-export function gameRules () {
-    console.log (
-      ` \n
-      GAME RULES:
-     1. Use small letters only;
-     2. One word movie titles are used.`
-    );
-  }
-  gameRules()
